c510da84f41cf32fba6293a57784a25ccd9fe95e
HarveyTung <haotingting30@gmail.com>
2025-02-02 10:17:17 +0800

    First commit

---
+++ docs/intro.md
@@ -0,0 +1,36 @@
+---
+title: Intro
+sidebar_position: 1
+---
+
+# TA Session Intro
+
+I believe many of you are confused about the requirement of building a project in this course.
+Especially this is a DBMS course, instead of Software Analysis & Design (SAD).
+
+There is a long long history of the existance of project in this course, and I'll skip that part.
+In sum, Prof. Chou believes this is a chance for you to prepare your project, plus, some students may not take the project in SAD course seriously.
+He therefore decided to setup this requirement.
+
+So SAD that you cannot suffer in SAD class only... don't be serious, just a lame joke.
+
+We, TAs, understand this is a HEAVY load for most of you, who may have no experience in coding a full-stack project.
+Therefore, we're here to help you.
+
+The TA sessions will teach you some essential skills you need to build a full-stack web application.
+
+:::important
+Disclaimer: The following is TA Harvey's personal opinion, another TA & Prof. may not agree
+
+Setting up the environment on your personal computer is a thing that may only learn by doing.
+
+I'll give you my settings and configs, if that doesn't work for you, you may want to look for the answer by yourself first.
+
+Specifically, in most cases, what we do is Google it in front of you.
+
+If you still cannot figure out, please tell me what is(are) the problem(s) and which methods you've tried and the results.
+
+By then, I'll try my best to help you.
+
+Good luck!
+:::
---
+++ docusaurus.config.js
@@ -0,0 +1,140 @@
+// @ts-check
+// `@type` JSDoc annotations allow editor autocompletion and type checking
+// (when paired with `@ts-check`).
+// There are various equivalent ways to declare your Docusaurus config.
+// See: https://docusaurus.io/docs/api/docusaurus-config
+
+import { themes as prismThemes } from "prism-react-renderer";
+
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: "Database Management Systems",
+  tagline: "DBMS 113-2",
+  favicon: "img/favicon.ico",
+
+  // Set the production url of your site here
+  url: "https://your-docusaurus-site.example.com",
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: "/",
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: "i-am-harveyt", // Usually your GitHub org/user name.
+  projectName: "113-2", // Usually your repo name.
+
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: "zh-Hant",
+    locales: ["zh-Hant", "en"],
+    localeConfigs: {
+      "zh-Hant": {
+        label: "Traditional Chinese",
+        path: "zh-Hant",
+      },
+      en: {
+        label: "English",
+        path: "en",
+      },
+    },
+  },
+
+  presets: [
+    [
+      "classic",
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: "./sidebars.js",
+          editUrl: "https://github.com/i-am-harveyt/dbms113-2/",
+        },
+        // blog: {
+        //   showReadingTime: true,
+        //   feedOptions: {
+        //     type: ["rss", "atom"],
+        //     xslt: true,
+        //   },
+        //   editUrl: "https://github.com/i-am-harveyt/dbms113-2/",
+        //   onInlineTags: "warn",
+        //   onInlineAuthors: "warn",
+        //   onUntruncatedBlogPosts: "warn",
+        // },
+        theme: {
+          customCss: "./src/css/custom.css",
+        },
+      }),
+    ],
+  ],
+
+  themes: ["@docusaurus/theme-mermaid"],
+  markdown: {
+    mermaid: true,
+  },
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      image: "img/docusaurus-social-card.jpg",
+      navbar: {
+        title: "DBMS 113-2",
+        logo: {
+          alt: "My Site Logo",
+          src: "img/logo.svg",
+        },
+        items: [
+          {
+            type: "docSidebar",
+            sidebarId: "tutorialSidebar",
+            position: "left",
+            label: "TA Session Tutorials",
+          },
+          {
+            type: "localeDropdown",
+            position: "left",
+          },
+        ],
+      },
+      footer: {
+        style: "dark",
+        links: [
+          {
+            title: "Class Website",
+            items: [
+              {
+                label: "Moodle",
+                href: "/docs/intro",
+              },
+              {
+                label: "Syllabus",
+                href: "/docs/intro",
+              },
+            ],
+          },
+          {
+            title: "More",
+            items: [
+              {
+                label: "GitHub",
+                href: "https://github.com/facebook/docusaurus",
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Harvey Tung. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: prismThemes.github,
+        darkTheme: prismThemes.dracula,
+      },
+    }),
+};
+
+export default config;
